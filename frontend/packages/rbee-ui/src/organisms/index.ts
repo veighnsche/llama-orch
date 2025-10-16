726e2554734d0f5a1402878214820333c9b1a004
Vince Liem <vincepaul.liem@gmail.com>
2025-10-16 20:07:41 +0200

    refactor: remove unused home page organisms and exports from index.ts

--- frontend/packages/rbee-ui/src/organisms/index.ts
+++ frontend/packages/rbee-ui/src/organisms/index.ts
@@ -2,20 +2,8 @@
 
 // Page-specific organisms
 export * from './CoreFeaturesTabs'
-export * from './Developers'
 export * from './Enterprise'
-export * from './FaqSection'
 export * from './Features'
-// Home page organisms (remaining - most migrated to templates)
-export * from './Home/CodeExamplesSection'
-export * from './Home/FeaturesSection'
-export * from './Home/FeatureTabsSection'
-export * from './Home/SocialProofSection'
-export type { Step as TimelineStep, StepsSectionProps } from './Home/StepsSection'
-// Steps section exports Step type
-export { StepsSection } from './Home/StepsSection'
-export * from './Home/TestimonialsRail'
-export * from './Home/TopologyDiagram'
 export * from './HowItWorksSection'
 // Pricing organisms migrated to templates
 // Providers exports (most migrated to templates)
